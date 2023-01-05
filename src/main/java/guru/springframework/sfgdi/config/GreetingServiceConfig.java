3709781ee596db73024d42c191711bf6a103a408
melifaroo <den.volgin@gmail.com>
2023-01-06 02:23:49 +0300

    Primary and Profile annotation in Configuration

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -2,14 +2,37 @@ package guru.springframework.sfgdi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 import guru.springframework.sfgdi.services.ConstructorGreetingService;
+import guru.springframework.sfgdi.services.I18NSpanishService;
+import guru.springframework.sfgdi.services.I18nEnglishGreetingService;
+import guru.springframework.sfgdi.services.PrimaryGreetingService;
 import guru.springframework.sfgdi.services.PropertyInjectedGreetingService;
 import guru.springframework.sfgdi.services.SetterInjectedGreetingService;
 
 @Configuration
 public class GreetingServiceConfig {
     
+    @Profile({"ES", "default"})
+    @Bean("i18nService")
+    I18NSpanishService i18NSpanishService(){
+        return new I18NSpanishService();
+    }
+
+    @Profile("EN")
+    @Bean("i18nService")
+    I18nEnglishGreetingService i18nEnglishGreetingService(){
+        return new I18nEnglishGreetingService();
+    }
+
+    @Primary
+    @Bean
+    PrimaryGreetingService primaryGreetingService(){
+        return new PrimaryGreetingService();
+    }
+
     @Bean
     ConstructorGreetingService constructorGreetingService(){
         return new ConstructorGreetingService();
